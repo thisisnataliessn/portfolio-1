8f8d90eb15cbe16d03ab3788481ee10850d3e5af
Natalie Zdan <natalie.zdan@gmail.com>
2019-02-24 18:46:15 -0600

    switched js tag order

--- js/main.js
+++ js/main.js
@@ -1,45 +1,54 @@
-  //Initiate carousel
-$('.main-carousel').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true
-  });
+  // //Initiate carousel
+  // $(document).ready(function () {
+  //   $('.carousel').flickity({
+  //     // options
+  //     cellAlign: 'left',
+  //     contain: true
+  //   });
+  // })
 
-
-
-
-// Find all YouTube videos
-var $allVideos = $("iframe[src^='//www.youtube.com']"),
-
-  // The element that is fluid width
-  $fluidEl = $(".main-topics");
-
-// Figure out and save aspect ratio for each video
-$allVideos.each(function () {
-
-  $(this)
-    .data('aspectRatio', this.height / this.width)
-
-    // and remove the hard coded width/height
-    .removeAttr('height')
-    .removeAttr('width');
-
+var $carousel = $('.carousel');
+// bind event listener first
+$carousel.on( 'ready.flickity', function() {
+  console.log('Flickity ready');
 });
+// initialize Flickity
+$carousel.flickity();
 
-// When the window is resized
-$(window).resize(function () {
 
-  var newWidth = $fluidEl.width();
 
-  // Resize all videos according to their own aspect ratio
+  // Find all YouTube videos
+  var $allVideos = $("iframe[src^='//www.youtube.com']"),
+
+    // The element that is fluid width
+    $fluidEl = $(".main-topics");
+
+  // Figure out and save aspect ratio for each video
   $allVideos.each(function () {
 
-    var $el = $(this);
-    $el
-      .width(newWidth)
-      .height(newWidth * $el.data('aspectRatio'));
+    $(this)
+      .data('aspectRatio', this.height / this.width)
+
+      // and remove the hard coded width/height
+      .removeAttr('height')
+      .removeAttr('width');
 
   });
 
-  // Kick off one resize to fix all videos on page load
-}).resize();
+  // When the window is resized
+  $(window).resize(function () {
+
+    var newWidth = $fluidEl.width();
+
+    // Resize all videos according to their own aspect ratio
+    $allVideos.each(function () {
+
+      var $el = $(this);
+      $el
+        .width(newWidth)
+        .height(newWidth * $el.data('aspectRatio'));
+
+    });
+
+    // Kick off one resize to fix all videos on page load
+  }).resize();
